2120fd25e527f6fe98c53ad236fc382bc08abff8
ellaverak <ella.korkeaaho@gmail.com>
2023-10-17 22:39:05 +0300

    Update urls.py

--- project/openid/urls.py
+++ project/openid/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("login/", views.login)
+    path('', views.home),
+    path('login/', views.login),
+    path('auth/', views.auth, name='auth'),
 ]
