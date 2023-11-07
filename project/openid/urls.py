97a75cf14833b0b977ac01d610030c3043f8c039
ellaverak <ella.korkeaaho@gmail.com>
2023-11-07 19:50:10 +0200

    jaaa

--- project/openid/urls.py
+++ project/openid/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.login_id, name='login_id'),
     path('auth/', views.auth, name='auth'),
     path('home/', views.home, name='home'),
-    path('logout/', views.logout, name='logout_id')
+    path('logout/', views.logout_id, name='logout_id')
 ]
