66cc91b5329ecc857d0f68ec93c4fd14fdfe2e79
ellaverak <ella.korkeaaho@gmail.com>
2023-11-07 17:23:51 +0200

    juujaa

--- project/project/settings.py
+++ project/project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import environ, os, sys
 
-sys.path.append('openid')
+#sys.path.append('openid')
 env = environ.Env()
 environ.Env.read_env()
 
