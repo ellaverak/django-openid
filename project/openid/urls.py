b57e87dc32bc522707423f9d27ec6e8ca8763172
ellaverak <ella.korkeaaho@gmail.com>
2023-12-10 21:07:00 +0200

    modify

--- project/openid/urls.py
+++ project/openid/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("login/", views.login, name="login"),
     path('auth/', views.auth, name='auth'),
     path('home/', views.home, name='home')
